e0c099d29a1cd1e9b50f2bec7fbd9547e57b4fef
ReactDev5 <reactdev5@acquaintsoft.com>
2025-09-19 12:02:34 +0530

    implement pages cms

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
     clientSegmentCache: true,
-    nodeMiddleware: true
-  }
+    nodeMiddleware: true,
+  },
+  reactStrictMode: true,
+  allowedDevOrigins: ["http://192.168.1.38:3000", "http://localhost:3000"],
 };
 
 export default nextConfig;
